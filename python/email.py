156f7e53a403a3e3a59814080cc44c4c120c48c8
tharukeakshan19@gmail.com <TharukaGit@12>
2026-05-02 06:56:26 +0530

    Add email handling functionality with Gmail integration and dotenv support

---
+++ python/email.py
@@ -0,0 +1,60 @@
+import imaplib
+import email
+import os
+import asyncio
+from dotenv import load_dotenv
+load_dotenv()
+
+EMAIL = os.getenv("EMAIL")
+APP_PASSWORD = os.getenv("APP_PASSWORD")
+
+try:
+    # Connect to Gmail
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(EMAIL, APP_PASSWORD)
+    mail.select("inbox")
+    print("Gmail connection created")
+except Exception as ex:
+    print("Gmail connection failed:", ex)
+    
+    
+async def realtime_reading_emails():
+    try:
+        while True:
+            try:
+                status, messages = mail.search(None, '(UNSEEN SUBJECT "purchase order")')
+                print("new email search: ", status)
+            except Exception as ex:
+                print("new email searching failed:", ex)
+            
+            
+            if messages[0]:
+                print(len(messages[0]), "found")
+                
+                for num in messages[0].split():
+                    status, msg_data = mail.fetch(num, '(RFC822)')
+                    
+                    msg = email.message_from_bytes(msg_data[0][1])
+                    
+                    print("checking attachemnets for email:", msg["Subject"])
+
+                    attachment_list = []
+                    for part in msg.walk():
+                        if part.get_content_disposition() == "attachment":
+                            filename = part.get_filename()
+                            
+                            if filename.endswith(".pdf"):
+                                # attachment_list.append(part.get_payload(decode=True))
+                                print(filename, "reading successfull")
+                            else:
+                                print(filename, "is not a pdf")
+                                continue
+                    else:
+                        mail.store(num, '+FLAGS', '\\Seen')
+            else:
+                print("No new emails found. try again after 10sec")
+                await asyncio.sleep(10)
+                continue
+            
+    except Exception as e:
+        print("realtime reading proccess failed:", e)
